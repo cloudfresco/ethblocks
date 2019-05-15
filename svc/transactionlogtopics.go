9313b1f6d9d6fc8a1568625fd4674b236ca41d13
rl26 <4592388+rl26@users.noreply.github.com>
2019-05-15 16:41:55 +0530

    Add PrintTopic function

--- svc/transactionlogtopics.go
+++ svc/transactionlogtopics.go
@@ -1,9 +1,16 @@
 package svc
 
 import (
+	"log"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 )
+
+// PrintTopic - Print Transaction Log Topic
+func PrintTopic(s common.Hash) {
+	log.Println("Topic  : ", s.Hex())
+}
 
 // GetTopics - Get Transaction Log Topic by log
 func GetTopics(lg *types.Log) []common.Hash {
